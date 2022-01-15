ef68d93b57be629d152ad3aa51f257647b831b7d
Cellie <CelliesProjects@users.noreply.github.com>
2022-01-15 14:03:08 +0100

    Fixes and formatting.

--- minimalUploadAuthESP32.ino
+++ minimalUploadAuthESP32.ino
@@ -10,11 +10,11 @@
 
 #include "upload_htm.h"
 
-const char* ssid          = "----------";
-const char* password      = "----------";
-const char* http_username = "admin";
-const char* http_password = "admin";
-const size_t MAX_FILESIZE = 1024 * 1024 * 5;
+const char* WIFI_SSID     = "----------";
+const char* WIFI_PASSWORD = "----------";
+const char* HTTP_USERNAME = "admin";
+const char* HTTP_PASSWORD = "admin";
+const size_t MAX_FILESIZE = 1024 * 1024 * 15;
 
 /* format bytes as KB, MB or GB string */
 String humanReadableSize(const size_t bytes) {
@@ -29,18 +29,19 @@ void setup() {
 
     WiFi.mode(WIFI_STA);
     WiFi.setSleep(false);
-    WiFi.begin(ssid, password);
-    Serial.printf("Connecting to SSID %s with PSK %s...\n", ssid, password);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.printf("Connecting to WIFI_SSID %s with PSK %s...\n", WIFI_SSID, WIFI_PASSWORD);
     if (WiFi.waitForConnectResult() != WL_CONNECTED) {
         Serial.printf("WiFi Failed!\n");
         return;
     }
 
     static AsyncWebServer server(80);
+    static const char* MIMETYPE_HTML{"text/html"};
 
     server.on("/", HTTP_GET, [](AsyncWebServerRequest * request)
     {
-        AsyncWebServerResponse *response = request->beginResponse_P(200, "text/html", upload_htm, upload_htm_len);
+        AsyncWebServerResponse *response = request->beginResponse_P(200, MIMETYPE_HTML, upload_htm, upload_htm_len);
         response->addHeader("Server", "ESP Async Web Server");
         request->send(response);
     });
@@ -57,7 +58,7 @@ void setup() {
 
     server.on("/api/upload", HTTP_POST, [](AsyncWebServerRequest * request)
     {
-        if (request->authenticate(http_username, http_password))
+        if (request->authenticate(HTTP_USERNAME, HTTP_PASSWORD))
             request->send(200);
         else {
             request->send(401);
@@ -66,7 +67,7 @@ void setup() {
     },
     [](AsyncWebServerRequest * request, String filename, size_t index, uint8_t *data, size_t len, bool final)
     {
-        if (!request->authenticate(http_username, http_password)) {
+        if (!request->authenticate(HTTP_USERNAME, HTTP_PASSWORD)) {
             request->send(401);
             request->client()->close();
             return;
@@ -77,26 +78,31 @@ void setup() {
         static time_t startTimer;
         if (!index) {
             startTimer = millis();
-            const char* fileSizeHeader{"FileSize"};
-            Serial.printf("UPLOAD: %s starting upload\n", filename.c_str());
-            if (request->hasHeader(fileSizeHeader)) {
-                Serial.printf("UPLOAD: fileSize:%s\n", request->header(fileSizeHeader));
-                if (request->header(fileSizeHeader).toInt() >= MAX_FILESIZE) {
-                    request->send(400, "text/html",
-                                  "Too large. (" + humanReadableSize(request->header(fileSizeHeader).toInt()) +
+            const char* FILESIZE_HEADER{"FileSize"};
+            Serial.printf("UPLOAD: Receiving: '%s'\n", filename.c_str());
+            if (request->hasHeader(FILESIZE_HEADER)) {
+                Serial.printf("UPLOAD: fileSize: %s\n", request->header(FILESIZE_HEADER));
+                if (request->header(FILESIZE_HEADER).toInt() >= MAX_FILESIZE) {
+                    request->send(400, MIMETYPE_HTML,
+                                  "Too large. (" + humanReadableSize(request->header(FILESIZE_HEADER).toInt()) +
                                   ") Max size is " + humanReadableSize(MAX_FILESIZE) + ".");
 
                     request->client()->close();
-                    Serial.printf("UPLOAD: Aborted upload of '%s' because file too big.\n", filename.c_str());
+                    Serial.printf("UPLOAD: Aborted upload because filesize limit.\n");
                     return;
                 }
+            } else {
+                request->send(400, MIMETYPE_HTML, "No filesize header present!");
+                request->client()->close();
+                Serial.printf("UPLOAD: Aborted upload because missing filesize header.\n");
+                return;
             }
         }
         //Store or do something with the data...
         //Serial.printf("file: '%s' received %i bytes\ttotal: %i\n", filename.c_str(), len, index + len);
 
         if (final)
-            Serial.printf("UPLOAD: %s Done. Received %i bytes in %.2fs which is %.2f kB/s.\n", filename.c_str(),
+            Serial.printf("UPLOAD: Done. Received %i bytes in %.2fs which is %.2f kB/s.\n",
                           index + len,
                           (millis() - startTimer) / 1000.0,
                           1.0 * (index + len) / (millis() - startTimer));
@@ -123,7 +129,7 @@ void setup() {
             Serial.printf("UNKNOWN");
 
         Serial.printf(" http://%s%s\n", request->host().c_str(), request->url().c_str());
-        request->send(404, "text/plain", "Not found.");
+        request->send(404, MIMETYPE_HTML, "404 - Not found.");
     });
 
     DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
